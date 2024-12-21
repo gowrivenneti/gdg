a047c637a300ecc05d70e7866f313fa9f1f5e821
gowrivenneti <lakshmigowrivenneti@gmail.com>
2024-12-21 15:17:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let btn = document.createElement("button");
+btn.innerText = "click me";
+btn.style.backgroundColor = "red";
+btn.style.color = "white";
+document.querySelector("body").prepend(btn);
+
